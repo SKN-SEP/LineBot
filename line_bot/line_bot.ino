ea061de8a2f7dd542eb3ae5b15111c97af77e846
AveePB <mike303.py@gmail.com>
2025-12-01 18:07:55 +0100

    Maxed out P & D terms at 100 duty cycle

--- line_bot/line_bot.ino
+++ line_bot/line_bot.ino
@@ -8,20 +8,20 @@ const uint8_t QTR_INPUT_PINS[8] = {12, 11, 7, 6, 5, 4, 3, 2}; /**< Line sensor i
 const uint8_t QTR_LEDON_PIN = 13; /**< Line sensor emitter pin */
 
 // PID constants
-const uint8_t BASE_SPEED = 70;
-const uint8_t MAX_SPEED = 100;
-const uint8_t MIN_SPEED = 0;
+const uint8_t BASE_SPEED = 90; /**< Normal motor duty cycle */
+const uint8_t MAX_SPEED = 120; /**< Maximum motor duty cycle */
+const uint8_t MIN_SPEED = 0; /**< Minimum motor duty cycle */
 
-const float KP = 0.6;
-const float KI = 0.0;
-const float KD = 1.0;
+const float KP = 0.03; /**< Propotional term, reacts how far you are off the line */
+const float KI = 0.0; /**< Intergral term, not used */ 
+const float KD = 0.1; /**< Derivative term, reacts to change in error */
 
 // Global variables
 mz_motors::Motor leftMT(mz_motors::ENA, mz_motors::IN1, mz_motors::IN2); /**< Left motor */
 mz_motors::Motor rightMT(mz_motors::ENB, mz_motors::IN3, mz_motors::IN4); /**< Right motor */
 uint16_t sensorValues[8];  /**< Sensor values */
 QTRSensors qtr8; /**< Line sensor */
-float prev_error;
+float prev_error; /**< Error from previous iteration */
 
 // Initializes variables
 void setup() {
@@ -46,8 +46,8 @@ void setup() {
   qtr8.setSensorPins(QTR_INPUT_PINS, 8);
   qtr8.setEmitterPin(QTR_LEDON_PIN);
 
-  // Calibrate (2.5ms/single read -> calibrates is 10 reads), total 10 seconds
-  for (int i=0; i<400; i++) qtr8.calibrate();
+  // Calibrate (2.5ms/single read -> calibrates is 10 reads), total 1.25 second
+  for (int i=0; i<50; i++) qtr8.calibrate();
   Serial.println("Completed configuring Qtr8");
   
   prev_error = 0;
@@ -61,12 +61,12 @@ void loop() {
   Serial.println(position);
 
   // Propotional term
-  float curr_error = position - 3500;
+  float curr_error = position - 3500.0;
   float p_out = curr_error * KP;
-  float i_out = 0 * KI;
   float d_out = (curr_error-prev_error) * KD;
 
-  int velocity = int(p_out + i_out + d_out);
+  int velocity = int(p_out + d_out);
+  prev_error = curr_error;
 
   // Update motors speed
   int vL = BASE_SPEED + velocity;
